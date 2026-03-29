b848a1c97f8a770b3ace7a7d7e36175142aa81e9
YohanaHiruy <johanhgf5@gmail.com>
2026-03-29 08:39:54 +0100

    password generator

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const textbox=document.getElementById("textbox");
-const tofah=document.getElementById("tofah");
-const tocel=document.getElementById("tocel");
-const result=document.getElementById("result");
+function generatepassword (length,includelowecase,includeuppercase,includenumbers,includesymbols){
+       
 
-let temp;
+    const lowercasechars="abcdefghijklmnopqrstuvwxyz"
+    const uppercasechars="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numberchars="0123456789"
+    const symbolchars="@#$%^@^&*_+"
 
-function convert(){
-    event.preventDefault();
-  if(tofah.checked){
-    temp=Number(textbox.value);
-    temp=temp * 9 / 5 + 32;
-    result.textContent=temp.toFixed(2)+"❄️F";
 
-  }
-  else if(tocel.checked){
-    temp=Number(textbox.value);
-    temp=(temp-32) * (5/9) ;
-   result.textContent=temp.toFixed(2) +"❄️C";
-  
-  }
-  else{
-     result.textContent="select a unit";
-  }
+    let allowedchars=""
+   let password="";
 
+   allowedchars+=includelowecase ?lowercasechars: ""
+      allowedchars+=includeuppercase ?uppercasechars: ""
+         allowedchars+=includesymbols ?symbolchars: ""
+            allowedchars+=includenumbers ?numberchars: ""
+if(length<0){
+    return `(password length must be atleast 1)`
 
 }
+if(allowedchars.length===0){
+    return`(tleast 1 set of characters needs to be selected) `
+
+}
+for(i=0; i<length; i++){
+    const randomindex=Math.floor(Math.random() * allowedchars.length);
+    password+=allowedchars[randomindex];
+}
+
+    return password;
+}
+
+
+const length=12;
+const includelowecase=true;
+const includeuppercase=true;
+const includenumbers=true;
+const includesymbols=true;
+
+const password=generatepassword(length,
+    includelowecase,
+    includeuppercase,
+    includenumbers,
+    includesymbols)
+
+    console.log(`generated password is :${password}`)
